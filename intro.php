f67499eb24a852e3f866f83a168155a0f2948e46
Andrias Meisyal <andriasonline@gmail.com>
2015-05-05 09:52:38 +0700

    add request a certificate intro

---
+++ intro.php
@@ -0,0 +1,11 @@
+<?php # script - intro.php
+include('includes/header.html');
+?>
+
+<h2></h2>
+<p>Untuk melakukan permintaan sertifikat digital, Anda diminta untuk mengisi detil
+informasi yang dibutuhkan pada halaman <a href="request_detail.php">pendaftaran</a>.</p>
+
+<?php
+include('includes/footer.html');
+?>
