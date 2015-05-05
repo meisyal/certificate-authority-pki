974d13bfbd07ce3d5f0b22e51f36916b9799c2f6
Andrias Meisyal <andriasonline@gmail.com>
2015-05-05 14:53:46 +0700

    add some sentence

--- intro.php
+++ intro.php
@@ -1,8 +1,10 @@
 <?php # script - intro.php
+
+$page_title = 'Penjelasan Alur Pendaftaran Sertifikat';
 include('includes/header.html');
 ?>
 
-<h2></h2>
+<h2>Alur Pengajuan Sertifikat Digital</h2>
 <p>Untuk melakukan permintaan sertifikat digital, Anda diminta untuk mengisi detil
 informasi yang dibutuhkan pada halaman <a href="request_detail.php">pendaftaran</a>.</p>
 
