f5713a3b78b6fbd24d47fc8d73f54830d61fe220
Andrias Meisyal <andriasonline@gmail.com>
2015-05-17 22:02:04 +0700

    update SQL statement

--- includes/login_functions.php
+++ includes/login_functions.php
@@ -35,11 +35,16 @@ function check_login($dbc, $username = '', $password = '') {
 
   // If everything's OK.
   if (empty($errors)) {
-    $q = "SELECT nama_user FROM pemohon WHERE nama_user='$u' AND kata_sandi= crypt('$p', kata_sandi) AND peranan = 'pemohon'";
-    $r = pg_query($dbc, $q);
+    // Prepare statement
+    $q = "SELECT nama_user FROM pemohon WHERE nama_user=$1 AND kata_sandi= crypt($2, kata_sandi) AND peranan = 'pemohon'";
 
-    $s = "SELECT nama FROM akun WHERE nama='$u' AND kata_sandi= crypt('$p', kata_sandi)";
-    $t = pg_query($dbc, $s);
+    $r = pg_prepare($dbc, "login_pemohon", $q);
+    $r = pg_execute($dbc, "login_pemohon", array($u, $p));
+
+    // Prepare another statement
+    $s = "SELECT nama FROM akun WHERE nama=$1 AND kata_sandi= crypt($2, kata_sandi)";
+    $t = pg_prepare($dbc, "login_admin", $s);
+    $t = pg_execute($dbc, "login_admin", array($u, $p));
 
     // Check the result
     if (pg_num_rows($r) == 1) {
